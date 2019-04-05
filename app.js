f5e2e739a211090fa541ec93990a0be697005545
jinkim <skinnyboykp@yahoo.com>
2019-04-04 20:57:05 -0700

    push

---
+++ app.js
@@ -0,0 +1,40 @@
+$("#recipe-app").on("click", function(){
+    window.location.href = "https://hmlauth.github.io/IngrediFeed/";
+});
+
+$("#Train-scheduler").on("click", function(){
+    window.location.href = " https://twitchjinja.github.io/train-scheduler";
+})
+
+$("#bamazon").on("click", function(){
+    window.location.href = " https://j.gifs.com/lx51DM.gif";
+})
+
+$("#bamazon2").on("click", function(){
+    window.location.href = " https://j.gifs.com/p8Q17V.gif";
+})
+
+$("#bamazon3").on("click", function(){
+    window.location.href = "  https://j.gifs.com/vlo1D5.gif";
+})
+
+$("#bamazon4").on("click", function(){
+    window.location.href = " https://j.gifs.com/q7Y1D0.gif";
+})
+
+$("#crystal").on("click", function(){
+    window.location.href = " https://twitchjinja.github.io/unit-4-game/";
+})
+
+$("#liri").on("click", function(){
+    window.location.href = " https://j.gifs.com/r80orK.gif";
+})
+
+$("#liri2").on("click", function(){
+    window.location.href = " https://j.gifs.com/zvmzJm.gif";
+})
+
+$("#liri3").on("click", function(){
+    window.location.href = "https://j.gifs.com/L7gzkA.gif";
+})
+
